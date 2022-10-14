56025c96f1b3bc0f55799b65a38484fcac7f822e
SiRi0000 <siriporn.senma@gmail.com>
2022-10-14 14:58:32 +0200

    Update ConcreteMember: add attribute id, constructor and implements getID()

--- src/org/hbrs/se1/ws22/uebung2/ConcreteMember.java
+++ src/org/hbrs/se1/ws22/uebung2/ConcreteMember.java
@@ -1,8 +1,12 @@
 package org.hbrs.se1.ws22.uebung2;
 
 public class ConcreteMember implements Member{
+    private Integer id;
+    public ConcreteMember(int id){
+      this.id = id;
+    }
     @Override
     public Integer getID() {
-        return null;
+        return id;
     }
 }
