f449cd3cabaf026e6e2c8c150157c2f551d02d38
SiRi0000 <siriporn.senma@gmail.com>
2022-10-18 06:47:49 +0200

    add comment

--- src/org/hbrs/se1/ws22/uebung2/Container.java
+++ src/org/hbrs/se1/ws22/uebung2/Container.java
@@ -1,10 +1,17 @@
 package org.hbrs.se1.ws22.uebung2;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
+
+//Funktion: Speichert Member-Objekte
 public class Container {
     private ArrayList<Member> speicher = new ArrayList<>();
+
+    /**
+     * @Funktion Die Methode fuegt Member ein, wenn dieser nicht bereits schon abgespeichert ist
+     * @param member
+     * @throws ContainerException, wenn Member bereits gespeichert wurde
+     */
     public void addMember(Member member) throws ContainerException{
         for (Member memberSpeicher : speicher){
             if(memberSpeicher.getID() != member.getID()){
@@ -14,6 +21,11 @@ public class Container {
         speicher.add(member);
     }
 
+    /**
+     * @Funktion: Die Methode Loescht Member uber die uebergebende ID
+     * @param id
+     * @return String
+     */
     public String deleteMember(Integer id){
         for(int i = 0; i < speicher.size(); i++){
             if(speicher.get(i).getID() == id){
@@ -25,11 +37,18 @@ public class Container {
     }
     // Keine Traceback, keine Konsolenausgabe über den Fehler, keine Einheitlichkeit der Fehlermeldung
 
+    /**
+     * @Funktion Die Methode gibt den letzten abgespeicherten Member auf der Konsole aus
+     */
     public void dump(){
         Member memberAktuell = speicher.get(speicher.size()-1);
         System.out.println(memberAktuell.toString());
     }
 
+    /**
+     * @Funktion Die Methode gibt die Anzahl der abgespeicherten Member im Container zurueck
+     * @return int
+     */
     public int size(){
         return speicher.size();
     }
